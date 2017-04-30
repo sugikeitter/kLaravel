fa4812ddbf56933cc35f7540abc594f3948252af
sugikeitter <s.l.n.f.13_kei.s@me.com>
2017-04-30 05:57:30 +0000

    Add select from table

--- myapp/app/Http/Controllers/MyUserController.php
+++ myapp/app/Http/Controllers/MyUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\MyUser;
+
 class MyUserController extends Controller
 {
     /**
@@ -14,7 +16,7 @@ class MyUserController extends Controller
     public function index()
     {
         $message = 'Hey!';
-        $data = [];
+        $data = MyUser::all();
         return $this->viewMyUser($message, $data);
     }
 
